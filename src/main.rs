a3a4e5de8fda956695afa1608c3775a0b6fa53fc
Mr. Door <mrdoor@protonmail.ch>
2024-08-07 01:36:01 +0200

    Further implementation

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
-use toml::Table;
-use std::fs::read_to_string;
+use clap::Parser;
+use log::LevelFilter;
+use simplelog::{CombinedLogger, ConfigBuilder, LevelPadding, SimpleLogger, WriteLogger};
+use std::fs::{read_to_string, File, OpenOptions};
+use std::io::{Error, Write};
+use std::path::PathBuf;
 use std::process::exit;
-use clap::{Parser};
+use std::str::FromStr;
+use std::{fs, io};
+use toml::Table;
 
 const OK: i32 = 0;
 const VALIDATION_FAIL: i32 = 1;
+const TARGET_NOT_FOUND: i32 = 2;
 const ROOT_DIR_DEFAULT: &str = ".notos";
-const CONFIG_FILE_NAME: &str = "config";
+const CONFIG_FILE_NAME: &str = "config.toml";
 const NOTES_DIR_DEFAULT: &str = "notes";
-const LOG_FILE_DEFAULT: &str = ".log";
-
+const LOG_FILE_DEFAULT: &str = "notos.log";
 const CONFIG_KEY_NOTES_DIR: &str = "notes_dir";
 const CONFIG_KEY_LOG_FILE: &str = "log_file";
 const CONFIG_KEY_LOG_ENABLED: &str = "log_enabled";
+const FILE_EXTENSION_TOPICS: &str = ".txt";
 
-
-#[derive(Parser)] // requires `derive` feature
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(long = "destroy", help = "Destroy (delete) a topic file. This can not be undone.")]
+    #[arg(
+        long = "destroy",
+        help = "Destroy (delete) a topic file. This can not be undone."
+    )]
     destroy: bool,
 
-    #[arg(short = 'd', long = "delete-line", help = "Delete line, starting from 0")]
+    #[arg(
+        short = 'd',
+        long = "delete-line",
+        help = "Delete line, starting from 0"
+    )]
     delete_line: bool,
 
-    #[arg(short = 'e', long = "edit", help = "Open the topic file in the default editor")]
+    #[arg(
+        short = 'e',
+        long = "edit",
+        help = "Open the topic file in the default editor"
+    )]
     edit: bool,
 
-    #[arg(short = 'a', long = "dump-all", help = "Prints all data from all topics")]
+    #[arg(
+        short = 'a',
+        long = "dump-all",
+        help = "Prints all data from all topics"
+    )]
     dump_all: bool,
 
     topic: Option<String>,
@@ -65,7 +86,9 @@ impl Cli {
             }
         } else if self.dump_all {
             if self.topic.is_some() || !self.value.is_empty() {
-                return Some("If you wish to dump all data then do not provide any further arguments");
+                return Some(
+                    "If you wish to dump all data then do not provide any further arguments",
+                );
             }
         }
         return None;
@@ -73,24 +96,33 @@ impl Cli {
 }
 
 struct Config {
-    notes_dir: String,
-    log_file: String,
+    notes_dir: PathBuf,
+    log_file: PathBuf,
+    log_level: String,
     log_enabled: bool,
 }
 
 fn fetch_config() -> Config {
     // Check if config file exists
     let home_dir = dirs::home_dir().expect("Could not find any home directory using dirs");
-    let home_dir_str = home_dir.to_str().expect("Failed to convert dir to string");
-    let config_file_path = home_dir.join(CONFIG_FILE_NAME);
+    let notos_root_dir = home_dir.join(ROOT_DIR_DEFAULT);
+    let config_file_path = notos_root_dir.join(CONFIG_FILE_NAME);
 
     let mut config = Config {
-        notes_dir: home_dir_str.to_owned() + "/" + ROOT_DIR_DEFAULT + "/" + NOTES_DIR_DEFAULT,
-        log_file: home_dir_str.to_owned() + "/" + ROOT_DIR_DEFAULT + "/" + LOG_FILE_DEFAULT,
+        notes_dir: notos_root_dir.join(NOTES_DIR_DEFAULT),
+        log_file: notos_root_dir.join(LOG_FILE_DEFAULT),
+        log_level: "debug".to_string(),
         log_enabled: true,
     };
 
-    if config_file_path.exists() {
+    if !notos_root_dir.exists() {
+        match fs::create_dir_all(&notos_root_dir) {
+            Ok(_) => {}
+            Err(e) => {
+                panic!("{}", e);
+            }
+        }
+    } else if config_file_path.exists() {
         // Load and read the file content
         let config_str = read_to_string(&config_file_path);
         match config_str {
@@ -101,30 +133,103 @@ fn fetch_config() -> Config {
                 let log_enabled = parsed.get(CONFIG_KEY_LOG_FILE);
 
                 if let Some(value) = notes_dir {
-                    config.notes_dir = value.as_str().unwrap().to_owned();
+                    config.notes_dir = PathBuf::from(value.as_str().unwrap().to_owned());
                 }
 
                 if let Some(value) = log_file {
-                    config.log_file = value.as_str().unwrap().to_owned();
+                    config.log_file = PathBuf::from(value.as_str().unwrap().to_owned());
                 }
 
                 if let Some(value) = log_enabled {
                     config.log_enabled = value.as_bool().unwrap();
                 }
-
             }
             _ => {}
         }
-    } 
+    }
     return config;
 }
 
+fn bootstrap_dirs_and_logger(config: &Config) {
+    if config.log_enabled {
+        let log_file: File;
+        if !config.log_file.exists() {
+            if let Some(parent_dir) = config.log_file.parent() {
+                if !parent_dir.exists() {
+                    fs::create_dir_all(parent_dir).expect("Failed to create log directory");
+                }
+            }
+            log_file = File::create(&config.log_file).expect("Failed to create log file");
+        } else {
+            log_file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open(&config.log_file)
+                .expect("Failed to open log file");
+        }
+
+        let log_config = ConfigBuilder::new()
+            .set_time_offset_to_local().expect("Failed to set local time for logger")
+            .set_target_level(LevelFilter::Off)
+            .set_location_level(LevelFilter::Off)
+            .set_thread_level(LevelFilter::Off)
+            .set_level_padding(LevelPadding::Right)
+            .build();
+        // Initialize the logger
+        CombinedLogger::init(vec![WriteLogger::new(
+            LevelFilter::from_str(config.log_level.as_str()).expect("Unknown log level"),
+            log_config,
+            log_file,
+        )])
+        .expect("Failed to initialize logger");
+    }
+    if !config.notes_dir.exists() {
+        fs::create_dir_all(&config.notes_dir).expect("Failed to create notes directory");
+    }
+}
+
+fn destroy(file: &PathBuf) -> io::Result<()> {
+    // Check if the path points to a file and not a directory
+    if file.is_file() {
+        // Attempt to remove the file
+        match fs::remove_file(&file) {
+            Ok(_) => {
+                log::info!("File '{}' deleted successfully.", file.display());
+                Ok(())
+            }
+            Err(e) => {
+                log::error!("Failed to delete file '{}': {}", file.display(), e);
+                Err(e)
+            }
+        }
+    } else {
+        // Return an error if the path is not a file
+        let err = Error::new(io::ErrorKind::InvalidInput, "Provided path is not a file");
+        log::error!("Failed to delete file '{}': {}", file.display(), err);
+        Err(err)
+    }
+}
+
+fn delete_line(path: &PathBuf, line_num: usize) {}
+
+fn add_to_file(path: &PathBuf, value: String) {}
+
+fn open_file_in_editor(path: &PathBuf) {}
+
+fn output_topic(path: &PathBuf) {}
+
+fn dump_all_file_data_for_dir(path: &PathBuf) {}
+
+fn print_all_topics_in_dir(path: &PathBuf) {}
+
 fn main() {
     let config = fetch_config();
+    bootstrap_dirs_and_logger(&config);
     let args = Cli::parse();
     let error_msg = args.validate();
     if error_msg.is_some() {
-        println!("{}", error_msg.unwrap());
+        log::error!("{}", error_msg.unwrap());
         exit(VALIDATION_FAIL);
     }
     println!("Destroy? {:?}", args.destroy);
@@ -137,14 +242,61 @@ fn main() {
     println!("log file: {:?}", config.log_file);
     println!("log enabled?: {:?}", config.log_enabled);
 
+    if args.topic.is_some() {
+        let target_topic = args.topic.unwrap();
+        let target_path = config
+            .notes_dir
+            .join(&target_topic)
+            .join(FILE_EXTENSION_TOPICS);
 
-    // No args, print list of topics, return
-    // Only topic arg, print it, return
-    // Topic and value arg -> add to topic -> fall-thru:
-    // edit flag -> open file in $EDITOR, return
-    // destroy flag -> destroy topic
-    // delete line flag -> delete line in topic
-    // dump-all flag
+        if !args.value.is_empty() {
+            // Both topic and value are present
+            // delete line flag -> delete line in topic
+            if args.delete_line {
+                delete_line(
+                    &target_path,
+                    args.value[0]
+                        .parse::<usize>()
+                        .expect("Failed to parse line number"),
+                );
+            } else {
+                // Add value to topic
+                add_to_file(&target_path, args.value.join(" "));
+            }
+        } else {
+            // Only topic is present
+            // destroy flag -> destroy topic
+            if args.destroy {
+                if target_path.exists() {
+                    destroy(&target_path).expect("Failed to destroy topic, see logs");
+                } else {
+                    log::warn!("Tried to destroy a non-existing topic {}", target_topic);
+                    exit(TARGET_NOT_FOUND);
+                }
+            }
+            // edit flag -> open file in $EDITOR, return
+            if args.edit {
+                open_file_in_editor(&target_path);
+            } else {
+                // Only topic arg, print it, return
+                output_topic(&target_path);
+            }
+        }
+    } else {
+        // Neither topic nor value are present
+        // dump-all flag
+        if args.dump_all {
+            dump_all_file_data_for_dir(&config.notes_dir);
+        } else {
+            // No args, print list of topics, return
+            print_all_topics_in_dir(&config.notes_dir);
+        }
+    }
 
+    log::trace!("This is a trace message."); // Won't be logged
+    log::debug!("This is a debug message."); // Won't be logged
+    log::info!("This is an info message."); // Should be logged
+    log::warn!("This is a warning message."); // Should be logged
+    log::error!("This is an error message."); // Should be logged
     exit(OK);
 }
